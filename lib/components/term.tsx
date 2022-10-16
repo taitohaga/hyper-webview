9dec145a4951f255465f5ca66a424beb09a30f43
taitohaga <taitohaga@gmail.com>
2022-10-16 16:57:50 +0900

    lint and fix chain options

--- lib/components/term.tsx
+++ lib/components/term.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { Terminal, ITerminalOptions, IDisposable } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
-import { WebLinksAddon } from 'xterm-addon-web-links';
-import { SearchAddon } from 'xterm-addon-search';
-import { WebglAddon } from 'xterm-addon-webgl';
-import { LigaturesAddon } from 'xterm-addon-ligatures';
-import { Unicode11Addon } from 'xterm-addon-unicode11';
-import { clipboard /*, shell */ } from 'electron';
+import {Terminal, ITerminalOptions, IDisposable} from 'xterm';
+import {FitAddon} from 'xterm-addon-fit';
+import {WebLinksAddon} from 'xterm-addon-web-links';
+import {SearchAddon} from 'xterm-addon-search';
+import {WebglAddon} from 'xterm-addon-webgl';
+import {LigaturesAddon} from 'xterm-addon-ligatures';
+import {Unicode11Addon} from 'xterm-addon-unicode11';
+import {clipboard /*, shell */} from 'electron';
 import Color from 'color';
 import terms from '../terms';
 import processClipboard from '../utils/paste';
 import SearchBox from './searchBox';
-import { TermProps } from '../hyper';
-import { ObjectTypedKeys } from '../utils/object';
+import {TermProps} from '../hyper';
+import {ObjectTypedKeys} from '../utils/object';
 
-const isWindows = ['Windows', 'Win16', 'Win32', 'WinCE'].includes(
-  navigator.platform
-);
+const isWindows = ['Windows', 'Win16', 'Win32', 'WinCE'].includes(navigator.platform);
 
 // map old hterm constants to xterm.js
 const CURSOR_STYLES = {
   BEAM: 'bar',
   UNDERLINE: 'underline',
-  BLOCK: 'block',
+  BLOCK: 'block'
 } as const;
 
 const isWebgl2Supported = (() => {
@@ -32,7 +30,7 @@ const isWebgl2Supported = (() => {
       const canvas = document.createElement('canvas');
       const gl = canvas.getContext('webgl2', {
         depth: false,
-        antialias: false,
+        antialias: false
       });
       isSupported = gl instanceof window.WebGL2RenderingContext;
     }
@@ -43,9 +41,7 @@ const isWebgl2Supported = (() => {
 const getTermOptions = (props: TermProps): ITerminalOptions => {
   // Set a background color only if it is opaque
   const needTransparency = Color(props.backgroundColor).alpha() < 1;
-  const backgroundColor = needTransparency
-    ? 'transparent'
-    : props.backgroundColor;
+  const backgroundColor = needTransparency ? 'transparent' : props.backgroundColor;
 
   return {
     macOptionIsMeta: props.modifierKeys.altIsMeta,
@@ -83,9 +79,9 @@ const getTermOptions = (props: TermProps): ITerminalOptions => {
       brightBlue: props.colors.lightBlue,
       brightMagenta: props.colors.lightMagenta,
       brightCyan: props.colors.lightCyan,
-      brightWhite: props.colors.lightWhite,
+      brightWhite: props.colors.lightWhite
     },
-    screenReaderMode: props.screenReaderMode,
+    screenReaderMode: props.screenReaderMode
   };
 };
 
@@ -102,23 +98,20 @@ export class WebView extends React.PureComponent<IWebViewProps> {
   }
 
   public goBack = (w: Electron.WebviewTag | null) => {
-    console.log(w);
-    if (w && w.canGoBack()) w.goBack();
+    if (w?.canGoBack()) w.goBack();
   };
 
   public goForward = (w: Electron.WebviewTag | null) => {
-    if (w && w.canGoForward()) w.goForward();
+    if (w?.canGoForward()) w.goForward();
   };
 
   public reload = (w: Electron.WebviewTag | null) => {
-    console.log(w);
-    w && w.reload();
+    w?.reload();
   };
 
   public openDevTools = (w: Electron.WebviewTag | null) => {
-    if (!w) return;
-    if (w.isDevToolsOpened()) w.closeDevTools();
-    else w.openDevTools();
+    if (w?.isDevToolsOpened()) w.closeDevTools();
+    else w?.openDevTools();
   };
 
   public render(): JSX.Element {
@@ -132,38 +125,26 @@ export class WebView extends React.PureComponent<IWebViewProps> {
           display: 'flex',
           flexDirection: 'column',
           width: '100%',
-          height: '100%',
+          height: '100%'
         }}
         onKeyPress={() => console.log('key pressed')}
       >
         <div
           style={{
             display: 'flex',
-            flexDirection: 'row',
+            flexDirection: 'row'
           }}
         >
-          <button
-            style={{ flexGrow: 1 }}
-            onClick={() => this.goBack(this.webviewRef.current)}
-          >
+          <button style={{flexGrow: 1}} onClick={() => this.goBack(this.webviewRef.current)}>
             Back
           </button>
-          <button
-            style={{ flexGrow: 1 }}
-            onClick={() => this.goForward(this.webviewRef.current)}
-          >
+          <button style={{flexGrow: 1}} onClick={() => this.goForward(this.webviewRef.current)}>
             Forward
           </button>
-          <button
-            style={{ flexGrow: 1 }}
-            onClick={() => this.reload(this.webviewRef.current)}
-          >
+          <button style={{flexGrow: 1}} onClick={() => this.reload(this.webviewRef.current)}>
             Reload
           </button>
-          <button
-            style={{ flexGrow: 1 }}
-            onClick={() => this.openDevTools(this.webviewRef.current)}
-          >
+          <button style={{flexGrow: 1}} onClick={() => this.openDevTools(this.webviewRef.current)}>
             Toggle Devtools
           </button>
         </div>
@@ -174,7 +155,7 @@ export class WebView extends React.PureComponent<IWebViewProps> {
             background: '#fff',
             display: 'inline-flex',
             width: '100%',
-            height: '100%',
+            height: '100%'
           }}
           src={this.props.src}
         />
@@ -195,7 +176,7 @@ export default class Term extends React.PureComponent<TermProps> {
   term!: Terminal;
   resizeObserver!: ResizeObserver;
   resizeTimeout!: NodeJS.Timeout;
-  state: { url: string | null };
+  state: {url: string | null};
   constructor(props: TermProps) {
     super(props);
     props.ref_(props.uid, this);
@@ -206,7 +187,7 @@ export default class Term extends React.PureComponent<TermProps> {
     this.termDefaultBellSound = null;
     this.fitAddon = new FitAddon();
     this.searchAddon = new SearchAddon();
-    this.state = { url: null };
+    this.state = {url: null};
   }
 
   // The main process shows this in the About dialog
@@ -215,12 +196,12 @@ export default class Term extends React.PureComponent<TermProps> {
     if (rendererTypes[uid] !== type) {
       rendererTypes[uid] = type;
       Term.rendererTypes = rendererTypes;
-      window.rpc.emit('info renderer', { uid, type });
+      window.rpc.emit('info renderer', {uid, type});
     }
   }
 
   componentDidMount() {
-    const { props } = this;
+    const {props} = this;
 
     this.termOptions = getTermOptions(props);
     this.term = props.term || new Terminal(this.termOptions);
@@ -228,9 +209,7 @@ export default class Term extends React.PureComponent<TermProps> {
 
     // The parent element for the terminal is attached and removed manually so
     // that we can preserve it across mounts and unmounts of the component
-    this.termRef = props.term
-      ? props.term.element!.parentElement!
-      : document.createElement('div');
+    this.termRef = props.term ? props.term.element!.parentElement! : document.createElement('div');
     this.termRef.className = 'term_fit term_term';
 
     this.termWrapperRef?.appendChild(this.termRef);
@@ -245,9 +224,7 @@ export default class Term extends React.PureComponent<TermProps> {
               'Falling back to canvas-based rendering.'
           );
         } else if (!isWebgl2Supported()) {
-          console.warn(
-            'WebGL2 is not supported on your machine. Falling back to canvas-based rendering.'
-          );
+          console.warn('WebGL2 is not supported on your machine. Falling back to canvas-based rendering.');
         } else {
           // Experimental WebGL renderer needs some more glue-code to make it work on Hyper.
           // If you're working on enabling back WebGL, you will also need to look into `xterm-addon-ligatures` support for that renderer.
@@ -256,15 +233,9 @@ export default class Term extends React.PureComponent<TermProps> {
       }
       Term.reportRenderer(props.uid, useWebGL ? 'WebGL' : 'Canvas');
 
-      const shallActivateWebLink = (
-        event: Record<string, any> | undefined
-      ): boolean => {
+      const shallActivateWebLink = (event: Record<string, any> | undefined): boolean => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return (
-          event &&
-          (!props.webLinksActivationKey ||
-            event[`${props.webLinksActivationKey}Key`])
-        );
+        return event && (!props.webLinksActivationKey || event[`${props.webLinksActivationKey}Key`]);
       };
 
       // eslint-disable-next-line @typescript-eslint/unbound-method
@@ -275,7 +246,7 @@ export default class Term extends React.PureComponent<TermProps> {
         new WebLinksAddon(
           (event: MouseEvent | undefined, uri: string) => {
             // if (shallActivateWebLink(event)) void shell.openExternal(uri);
-            this.setState({ url: uri });
+            this.setState({url: uri});
           },
           {
             // prevent default electron link handling to allow selection, e.g. via double-click
@@ -283,7 +254,7 @@ export default class Term extends React.PureComponent<TermProps> {
               event?.preventDefault();
               return shallActivateWebLink(event);
             },
-            priority: Date.now(),
+            priority: Date.now()
           }
         )
       );
@@ -315,8 +286,7 @@ export default class Term extends React.PureComponent<TermProps> {
     if (props.onActive) {
       this.term.textarea?.addEventListener('focus', props.onActive);
       this.disposableListeners.push({
-        dispose: () =>
-          this.term.textarea?.removeEventListener('focus', this.props.onActive),
+        dispose: () => this.term.textarea?.removeEventListener('focus', this.props.onActive)
       });
     }
 
@@ -326,7 +296,7 @@ export default class Term extends React.PureComponent<TermProps> {
 
     if (props.onResize) {
       this.disposableListeners.push(
-        this.term.onResize(({ cols, rows }) => {
+        this.term.onResize(({cols, rows}) => {
           props.onResize(cols, rows);
         })
       );
@@ -339,20 +309,12 @@ export default class Term extends React.PureComponent<TermProps> {
       this.disposableListeners.push(
         this.term.onCursorMove(() => {
           const cursorFrame = {
-            x:
-              this.term.buffer.active.cursorX *
-              (this.term as any)._core._renderService.dimensions
-                .actualCellWidth,
-            y:
-              this.term.buffer.active.cursorY *
-              (this.term as any)._core._renderService.dimensions
-                .actualCellHeight,
-            width: (this.term as any)._core._renderService.dimensions
-              .actualCellWidth,
-            height: (this.term as any)._core._renderService.dimensions
-              .actualCellHeight,
+            x: this.term.buffer.active.cursorX * (this.term as any)._core._renderService.dimensions.actualCellWidth,
+            y: this.term.buffer.active.cursorY * (this.term as any)._core._renderService.dimensions.actualCellHeight,
+            width: (this.term as any)._core._renderService.dimensions.actualCellWidth,
+            height: (this.term as any)._core._renderService.dimensions.actualCellHeight,
             col: this.term.buffer.active.cursorX,
-            row: this.term.buffer.active.cursorY,
+            row: this.term.buffer.active.cursorY
           };
           props.onCursorMove?.(cursorFrame);
         })
@@ -360,7 +322,7 @@ export default class Term extends React.PureComponent<TermProps> {
     }
 
     window.addEventListener('paste', this.onWindowPaste, {
-      capture: true,
+      capture: true
     });
 
     terms[this.props.uid] = this;
@@ -465,8 +427,7 @@ export default class Term extends React.PureComponent<TermProps> {
 
     // Use bellSound in nextProps if it exists
     // otherwise use the default sound found in xterm.
-    nextTermOptions.bellSound =
-      this.props.bellSound || this.termDefaultBellSound!;
+    nextTermOptions.bellSound = this.props.bellSound || this.termDefaultBellSound!;
 
     if (!prevProps.search && this.props.search) {
       this.search();
@@ -474,21 +435,13 @@ export default class Term extends React.PureComponent<TermProps> {
 
     // Update only options that have changed.
     ObjectTypedKeys(nextTermOptions)
-      .filter(
-        option =>
-          option !== 'theme' &&
-          nextTermOptions[option] !== this.termOptions[option]
-      )
-      .forEach(option => {
+      .filter((option) => option !== 'theme' && nextTermOptions[option] !== this.termOptions[option])
+      .forEach((option) => {
         try {
           this.term.setOption(option, nextTermOptions[option]);
         } catch (_e) {
-          const e = _e as { message: string };
-          if (
-            /The webgl renderer only works with the webgl char atlas/i.test(
-              e.message
-            )
-          ) {
+          const e = _e as {message: string};
+          if (/The webgl renderer only works with the webgl char atlas/i.test(e.message)) {
             // Ignore this because the char atlas will also be changed
           } else {
             throw e;
@@ -501,8 +454,7 @@ export default class Term extends React.PureComponent<TermProps> {
       !this.termOptions.theme ||
       nextTermOptions.rendererType !== this.termOptions.rendererType ||
       ObjectTypedKeys(nextTermOptions.theme!).some(
-        option =>
-          nextTermOptions.theme![option] !== this.termOptions.theme![option]
+        (option) => nextTermOptions.theme![option] !== this.termOptions.theme![option]
       );
     if (shouldUpdateTheme) {
       this.term.setOption('theme', nextTermOptions.theme);
@@ -520,10 +472,7 @@ export default class Term extends React.PureComponent<TermProps> {
       this.fitResize();
     }
 
-    if (
-      prevProps.rows !== this.props.rows ||
-      prevProps.cols !== this.props.cols
-    ) {
+    if (prevProps.rows !== this.props.rows || prevProps.cols !== this.props.cols) {
       this.resize(this.props.cols!, this.props.rows!);
     }
   }
@@ -553,11 +502,11 @@ export default class Term extends React.PureComponent<TermProps> {
     // instead of invoking `destroy`, since it will make the
     // term insta un-attachable in the future (which we need
     // to do in case of splitting, see `componentDidMount`
-    this.disposableListeners.forEach(handler => handler.dispose());
+    this.disposableListeners.forEach((handler) => handler.dispose());
     this.disposableListeners = [];
 
     window.removeEventListener('paste', this.onWindowPaste, {
-      capture: true,
+      capture: true
     });
   }
 
@@ -565,7 +514,7 @@ export default class Term extends React.PureComponent<TermProps> {
     return (
       <div
         className={`term_fit ${this.props.isTermActive ? 'term_active' : ''}`}
-        style={{ padding: this.props.padding }}
+        style={{padding: this.props.padding}}
         onMouseUp={this.onMouseUp}
       >
         {this.state.url ? (
@@ -573,10 +522,7 @@ export default class Term extends React.PureComponent<TermProps> {
         ) : (
           <>
             {this.props.customChildrenBefore}
-            <div
-              ref={this.onTermWrapperRef}
-              className="term_fit term_wrapper"
-            />
+            <div ref={this.onTermWrapperRef} className="term_fit term_wrapper" />
             {this.props.customChildren}
             {this.props.search ? (
               <SearchBox
